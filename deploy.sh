b884779d66ceb4e8f00cbb6ec9a05b4113dabb59
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-11 23:41:48 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,31 @@
-# script to deploy to GitHub Page
-yarn
+set -eu
+
+##################################
+# Script to deploy to GitHub Page
+##################################
+
+# save current branch name to come back after
+BRANCH=$(git rev-parse --abbrev-ref HEAD);
+ORIGIN=$(git remote -v | sed -n 's/^deploy\s//p');
+
+echo "$ORIGIN";
+
+if [ -z "$ORIGIN" ]; then
+  echo "register remote repository named 'deploy' for github pages";
+  exit 1;
+fi
+
+# prepare temporary branch
 git branch -D deploy;
 git checkout -b deploy;
+# build js file
 yarn build:prod;
 cp dist/bundle.min.js docs/bundle.min.js;
-git add docs/bundle.min.js;
-git commit -m "update bundle.js";
+# git add and commit updated files
+sed -i -e 's,/docs/bundle.min.js,,g' .gitignore
+git add .gitignore docs/bundle.min.js;
+git commit -m "commit by deploy script";
+# deploy to GitHub Page
 git push -f deploy deploy:master;
+# back to master branch
+git checkout -f master
