37c65ad3eee93492c8d2f66d9e7d5c549593c2f3
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2020-01-11 23:48:55 +0900

    make variable

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,28 @@ set -eu
 ##################################
 
 # save current branch name to come back after
-BRANCH=$(git rev-parse --abbrev-ref HEAD);
-ORIGIN=$(git remote -v | sed -n 's/^deploy\s//p');
+WORKING_BRANCH=$(git rev-parse --abbrev-ref HEAD);
+DEPLOY_BRANCH=deploy;
+DEPLOY_ORIGIN=deploy;
+ORIGIN_CHECK=$(git remote -v | sed -n 's/^deploy\s//p');
 
-echo "$ORIGIN";
-
-if [ -z "$ORIGIN" ]; then
-  echo "register remote repository named 'deploy' for github pages";
+# if origin is not registered, exit
+if [ -z "$ORIGIN_CHECK" ]; then
+  echo "register remote repository named '$DEPLOY_ORIGIN' for github pages";
   exit 1;
 fi
 
 # prepare temporary branch
-git branch -D deploy;
-git checkout -b deploy;
+git branch -D $DEPLOY_BRANCH;
+git checkout -b $DEPLOY_BRANCH;
 # build js file
 yarn build:prod;
 cp dist/bundle.min.js docs/bundle.min.js;
 # git add and commit updated files
-sed -i -e 's,/docs/bundle.min.js,,g' .gitignore
+sed -i -e 's,/docs/bundle.min.js,,g' .gitignore;
 git add .gitignore docs/bundle.min.js;
 git commit -m "commit by deploy script";
 # deploy to GitHub Page
-git push -f deploy deploy:master;
+git push -f $DEPLOY_ORIGIN "$DEPLOY_BRANCH:master";
 # back to master branch
-git checkout -f $BRANCH;
+git checkout -f $WORKING_BRANCH;
